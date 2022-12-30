bb171e50b7704b8fcd3ca6298fc117557807debf
Gia <gia.jihye.j@gmail.com>
2022-12-30 17:20:41 +0900

    초기 세팅

---
+++ components/Rank.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Rank = () => {
+  return (
+    <section>Rank</section>
+  )
+}
+
+export default Rank
---
+++ components/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchInput from './SearchInput'
+import SearchResult from './SearchResult'
+
+const Search = () => {
+  return (
+    <section>
+    <SearchInput/>
+    <SearchResult/>
+    </section>
+  )
+}
+
+export default Search
