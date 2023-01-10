92b6b4087eac5dc9baa6f5a4cbcc9532889ae812
Gia <gia.jihye.j@gmail.com>
2023-01-10 17:46:46 +0900

    ticker css

--- components/Rank.style.ts
+++ components/Rank.style.ts
@@ -5,7 +5,7 @@ export const Rank = styled.aside`
   overflow: hidden;
   border: 1px solid #bbb;
   ul {
-    animation: slideup 10s infinite;
+    animation: slideup 20s infinite;
     li {
       height: 34px;
       padding: 0 1rem;
@@ -16,12 +16,45 @@ export const Rank = styled.aside`
   a {
     text-decoration: none;
   }
-  @keyframes slideup {
-    0% {
-      transform: translateY(0);
+    @keyframes slideup {
+      0% {
+        transform: translateY(0);
+      }
+      8% {
+        transform: translateY(0);
+      }
+      10%{
+        transform: translateY(calc(var(--h) * -1));
+      }
+      18%{
+        transform: translateY(calc(var(--h) * -1));
+      }
+      20%{
+        transform: translateY(calc(var(--h) * -2));
+      }
+      28%{
+        transform: translateY(calc(var(--h) * -2));
+      }
+      30%{
+        transform: translateY(calc(var(--h) * -3));
+      }
+      38%{
+        transform: translateY(calc(var(--h) * -3));
+      }
+      40%{
+        transform: translateY(calc(var(--h) * -4));
+      }
+      48%{
+        transform: translateY(calc(var(--h) * -4));
+      }
+      50%{
+        transform: translateY(calc(var(--h) * -5));
+      }
+      58%{
+        transform: translateY(calc(var(--h) * -5));
+      }
+      100% {
+        transform: translateY(calc(var(--h) * -9));
+      }
     }
-    100% {
-      transform: translateY(var(--h) * -9);
-    }
-  }
 `;
